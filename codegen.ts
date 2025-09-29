ffb5784a3b1f22d9568162f868a54383600d3713
ValentinZoia <valentinzoia@gmail.com>
2025-09-28 23:28:11 -0300

    fix(change the config, add useTypeImports)

--- codegen.ts
+++ codegen.ts
@@ -2,14 +2,26 @@ import type { CodegenConfig } from "@graphql-codegen/cli";
 
 const config: CodegenConfig = {
   schema: "http://localhost:3000/graphql",
-  documents: ["src/graphql/**/*.tsx"],
+  documents: ["src/graphql/**/*.ts"],
   ignoreNoDocuments: true, // for better experience with the watcher
   generates: {
     "./src/gql/": {
       preset: "client",
-      plugins: ["typescript"],
+      config: {
+        useTypeImports: true,
+      },
     },
   },
 };
 
 export default config;
+
+/*
+GraphQL Code Generator provides a unified way 
+to get TypeScript types from GraphQL operations
+*/
+//Writing Graphql Queries
+//npx graphql-codegen --watch --verbose
+//npx graphql-codegen --config codegen.ts
+
+//https://the-guild.dev/graphql/codegen/docs/guides/react-vue
