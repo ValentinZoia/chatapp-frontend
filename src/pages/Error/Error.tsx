2d5343b9379b70225e252cb99f147fdebc53ed1b
ValentinZoia <valentinzoia@gmail.com>
2025-09-29 21:32:33 -0300

    feat(create the pages, Home, ChatRoom and Error)

---
+++ src/pages/Error/Error.tsx
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom";
+
+const Error = () => {
+  return (
+    <>
+      <div className="w-full min-h-screen flex flex-col">
+        <div className="w-full flex-1 flex flex-col gap-4 justify-center items-center">
+          <h2 className="text-2xl font-bold">
+            Parece que esta página no existe
+          </h2>
+          <Link to="/" className="text-muted-foreground ml-2">
+            Volver al inicio
+          </Link>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Error;
