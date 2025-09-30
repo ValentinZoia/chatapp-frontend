5aa67bbc04851be8a45833d77dc6bccc9d38c558
ValentinZoia <valentinzoia@gmail.com>
2025-09-29 21:34:48 -0300

    some barrel files

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,16 @@
+import { RouterProvider } from "react-router-dom";
 import "./App.css";
-import { Button } from "./components/ui/button";
-import { useQuery } from "@apollo/client/react";
-import { GET_HELLO } from "./graphql/queries/Hello";
-import FindUserById from "./components/find-user-id";
-import { Card, CardHeader, CardTitle, CardContent } from "./components/ui/card";
-import CreateUser from "./components/create-user";
+import router from "./routes";
+// import { LoginModal } from "./components/LoginModal";
+// import CreateRoomModal from "./components/CreateRoomModal/CreateRoomModal";
 
 function App() {
-  const { loading, error, data } = useQuery<{ hello: string }>(GET_HELLO);
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error : {error.message}</p>;
-  console.log(data);
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center">
-      <h1 className="text-3xl text-red-500 ">Hola maquinas!</h1>
-
-      <Button>Button</Button>
-
-      {data && data.hello}
-      <FindUserById id={1} />
-      <Card className="w-full max-w-md mx-auto">
-        <CardHeader>
-          <CardTitle>Create Account</CardTitle>
-        </CardHeader>
-        <CardContent>
-          {/* Form */}
-          <CreateUser />
-        </CardContent>
-      </Card>
-    </div>
+    <>
+      <RouterProvider router={router} />
+      {/* <LoginModal />
+      <CreateRoomModal /> */}
+    </>
   );
 }
 
