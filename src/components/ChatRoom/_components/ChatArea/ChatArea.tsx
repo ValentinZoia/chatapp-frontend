e77426154c17085516df9412925f333f3c48c470
ValentinZoia <valentinzoia@gmail.com>
2025-10-02 21:40:32 -0300

    feat(delete mock messages object, and use real messages for props)

--- src/components/ChatRoom/_components/ChatArea/ChatArea.tsx
+++ src/components/ChatRoom/_components/ChatArea/ChatArea.tsx
@@ -1,21 +1,51 @@
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { Message } from "../Message";
-import { EXAMPLE_MESSAGES } from "@/data/example-messages";
+import { useEffect, useRef } from "react";
 
-function ChatArea() {
+import type { IMessage } from "@/data/Chatrooms/useGetMessagesForChatroom";
+
+interface ChatAreaProps {
+  messages: IMessage[];
+  currentUserId: number;
+}
+
+function ChatArea({ messages, currentUserId }: ChatAreaProps) {
+  const scrollAreaRef = useRef<HTMLDivElement>(null);
+  console.log(currentUserId);
+  const scrollToBottom = () => {
+    if (scrollAreaRef.current) {
+      scrollAreaRef.current.scrollTo({
+        top: scrollAreaRef.current.scrollHeight,
+        behavior: "smooth",
+      });
+    }
+  };
+
+  useEffect(() => {
+    scrollToBottom();
+  }, [messages]);
+
   return (
-    <ScrollArea className="flex-1 p-6">
+    <ScrollArea className="flex-1 p-6" ref={scrollAreaRef}>
       <div className="space-y-4">
-        {EXAMPLE_MESSAGES.map((msg) => (
-          <div
-            key={msg.id}
-            className={`flex gap-3 ${
-              msg.isOwn ? "flex-row-reverse" : "flex-row"
-            }`}
-          >
-            <Message msg={msg} />
-          </div>
-        ))}
+        {messages && messages.length > 0 ? (
+          messages.map((msg) => (
+            <div
+              key={msg?.id}
+              className={`flex gap-3 ${
+                msg?.user?.id === currentUserId
+                  ? "flex-row-reverse"
+                  : "flex-row"
+              }`}
+            >
+              <Message msg={msg} isOwn={msg?.user?.id === currentUserId} />
+            </div>
+          ))
+        ) : (
+          <>
+            <p>No hay mensajes Todavia. Se el primero en enviar uno!</p>
+          </>
+        )}
       </div>
     </ScrollArea>
   );
