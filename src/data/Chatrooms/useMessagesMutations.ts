de997201efde25573740f20fdb96b799f88eb586
ValentinZoia <valentinzoia@gmail.com>
2025-10-02 21:52:20 -0300

    feat(create the hook to manage all mutations from Messages feature)

---
+++ src/data/Chatrooms/useMessagesMutations.ts
@@ -0,0 +1,99 @@
+import {
+  SEND_MESSAGE,
+  USER_STARTED_TYPING_MUTATION,
+  USER_STOPPED_TYPING_MUTATION,
+} from "@/graphql/mutations";
+
+import type {
+  SendMessageMutation,
+  SendMessageMutationVariables,
+  UserStartedTypingMutationMutation,
+  UserStartedTypingMutationMutationVariables,
+  UserStoppedTypingMutationMutation,
+  UserStoppedTypingMutationMutationVariables,
+} from "@/gql/graphql";
+
+import { useMutation } from "@apollo/client/react";
+import { GET_CHATROOMS_FOR_USER } from "@/graphql/queries";
+
+export function useMessagesMutations(userId?: number) {
+  //send Message
+  const [
+    sendMessageMutation,
+    {
+      loading: sendMessageLoading,
+      error: sendMessageError,
+      data: sendMessageData,
+    },
+  ] = useMutation<SendMessageMutation, SendMessageMutationVariables>(
+    SEND_MESSAGE,
+    {
+      refetchQueries: [
+        {
+          query: GET_CHATROOMS_FOR_USER,
+          variables: { userId },
+        },
+      ],
+    }
+  );
+
+  const sendMessage = (input: SendMessageMutationVariables) => {
+    return sendMessageMutation({ variables: input });
+  };
+
+  //started Typing
+
+  const [
+    startedTypingMutation,
+    {
+      loading: startedTypingLoading,
+      error: startedTypingError,
+      data: startedTypingData,
+    },
+  ] = useMutation<
+    UserStartedTypingMutationMutation,
+    UserStartedTypingMutationMutationVariables
+  >(USER_STARTED_TYPING_MUTATION);
+
+  const startedTyping = (input: UserStartedTypingMutationMutationVariables) => {
+    return startedTypingMutation({ variables: input });
+  };
+
+  //stopped typing
+
+  const [
+    stoppedTypingMutation,
+    {
+      loading: stoppedTypingLoading,
+      error: stoppedTypingError,
+      data: stoppedTypingData,
+    },
+  ] = useMutation<
+    UserStoppedTypingMutationMutation,
+    UserStoppedTypingMutationMutationVariables
+  >(USER_STOPPED_TYPING_MUTATION);
+
+  const stoppedTyping = (input: UserStoppedTypingMutationMutationVariables) => {
+    return stoppedTypingMutation({ variables: input });
+  };
+
+  return {
+    //send Message
+    sendMessage,
+    sendMessageLoading,
+    sendMessageError,
+    sendMessageData,
+
+    //started typing
+    startedTyping,
+    startedTypingLoading,
+    startedTypingError,
+    startedTypingData,
+
+    //stopped typing
+    stoppedTyping,
+    stoppedTypingLoading,
+    stoppedTypingError,
+    stoppedTypingData,
+  };
+}
