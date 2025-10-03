7e9f66e6c50c00cbb4e011097aef4968c0ffa57f
ValentinZoia <valentinzoia@gmail.com>
2025-10-02 21:47:38 -0300

    feat(create a UserRooms component. Get the data from backend)

---
+++ src/components/Sidebar/_components/UserRooms/UserRooms.tsx
@@ -0,0 +1,30 @@
+import { SidebarGroup, SidebarSeparator } from "@/components/ui/sidebar";
+import { useGetChatroomsForUser } from "@/data/Chatrooms/useGetChatroomsForUser";
+import { useUserStore } from "@/stores/userStore";
+import { ItemRoomList } from "../ItemRoomList";
+
+function UserRooms() {
+  const userId = useUserStore((state) => state.id);
+
+  const { data: userRooms, error } = useGetChatroomsForUser(userId as number);
+  if (error) {
+    console.log(error);
+  }
+
+  return (
+    <>
+      {userRooms && userRooms.getChatroomsForUser.length > 0 && (
+        <>
+          <SidebarSeparator />
+          <SidebarGroup>
+            <ItemRoomList
+              rooms={userRooms.getChatroomsForUser}
+              title="Mis Salas"
+            />
+          </SidebarGroup>
+        </>
+      )}
+    </>
+  );
+}
+export default UserRooms;
