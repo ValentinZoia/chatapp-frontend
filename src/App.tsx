49675a36ac571af8494ec291cce5af4ee7662970
ValentinZoia <valentinzoia@gmail.com>
2025-10-02 21:09:24 -0300

    refactor(delete comments)

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import { RouterProvider } from "react-router-dom";
 import "./App.css";
 import router from "./routes";
-// import { LoginModal } from "./components/LoginModal";
-// import CreateRoomModal from "./components/CreateRoomModal/CreateRoomModal";
+import { LoginModal } from "./components/Auth/LoginModal";
+import CreateRoomModal from "./components/CreateRoomModal/CreateRoomModal";
+import { RouterProvider } from "react-router-dom";
 
 function App() {
   return (
     <>
       <RouterProvider router={router} />
-      {/* <LoginModal />
-      <CreateRoomModal /> */}
+      <LoginModal />
+      <CreateRoomModal />
     </>
   );
 }
