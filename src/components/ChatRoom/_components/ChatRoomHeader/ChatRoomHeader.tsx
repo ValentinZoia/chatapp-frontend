d1050b6e0cc6a19313307985c288073bbe1daa0e
ValentinZoia <valentinzoia@gmail.com>
2025-10-02 22:13:17 -0300

    refactor(coment unused data)

--- src/components/ChatRoom/_components/ChatRoomHeader/ChatRoomHeader.tsx
+++ src/components/ChatRoom/_components/ChatRoomHeader/ChatRoomHeader.tsx
@@ -1,10 +1,16 @@
 import { BadgeUsersActives } from "@/components/BadgeUsersActives";
-import type { IRoom } from "@/components/Sidebar/_components/ItemRoomList/ItemRoomList";
+// import type { IRoom } from "@/components/Sidebar/_components/ItemRoomList/ItemRoomList";
 import { Button } from "@/components/ui/button";
 import { ArrowLeft } from "lucide-react";
 import { Link } from "react-router-dom";
-import { Image } from "@/components/Image";
-function ChatRoomHeader({ roomInfo }: { roomInfo: IRoom }) {
+// import { Image } from "@/components/Image";
+function ChatRoomHeader({
+  // roomInfo,
+  liveUsers,
+}: {
+  // roomInfo: IRoom;
+  liveUsers: number;
+}) {
   return (
     <div className="flex items-center justify-between border-b border-border bg-card px-6 py-4">
       <div className="flex items-center gap-4">
@@ -15,9 +21,10 @@ function ChatRoomHeader({ roomInfo }: { roomInfo: IRoom }) {
         </Link>
         <div className="flex items-center gap-3">
           <div
-            className={`h-10 w-10 rounded-lg ${roomInfo.color} flex items-center justify-center`}
+            className={`h-10 w-10 rounded-lg  flex items-center justify-center`}
           >
-            {roomInfo.image ? (
+            <h1>logo</h1>
+            {/* {roomInfo.image ? (
               <Image
                 src={roomInfo.image}
                 alt={roomInfo.name}
@@ -25,24 +32,27 @@ function ChatRoomHeader({ roomInfo }: { roomInfo: IRoom }) {
                 height={40}
                 placeholderSrc="https://placehold.co/30"
                 errorSrc="https://placehold.co/30"
-              />
-            ) : (
+              /> */}
+            {/* ) : (
               <span className="text-lg font-bold text-white">
-                {roomInfo.name.charAt(0)}
-              </span>
-            )}
+                {/* {roomInfo.name.charAt(0)} */}
+            {/* S
+              </span> */}
+            {/* )}  */}
           </div>
           <div>
             <h1 className="text-lg font-semibold text-foreground">
-              {roomInfo.name}
+              {/* {roomInfo.name} */}
+              Sala Genrica
             </h1>
             <p className="text-sm text-muted-foreground">
-              {roomInfo.description}
+              {/* {roomInfo.description} */}
+              Desc Genrica
             </p>
           </div>
         </div>
       </div>
-      <BadgeUsersActives usersAvtives={9} />
+      <BadgeUsersActives usersAvtives={liveUsers} />
     </div>
   );
 }
