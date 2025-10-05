f0b1022e858a6148a1f978b342d25b7a6c219f25
ValentinZoia <valentinzoia@gmail.com>
2025-10-05 17:48:08 -0300

    refactor(change the function name)

--- src/data/Users/useGetUsersOfChatroom.ts
+++ src/data/Users/useGetUsersOfChatroom.ts
@@ -1,15 +1,15 @@
 import { useSuspenseQuery } from "@apollo/client/react";
-import { GET_USERS_OF_CHATROOM } from "@/graphql/queries";
+import { GET_USERS_AND_CHATROOM_INFO } from "@/graphql/queries";
 import type {
   GetUsersOfChatroomQuery,
   GetUsersOfChatroomQueryVariables,
 } from "@/gql/graphql";
 
-export function useGetUsersOfChatroom(chatroomId: number) {
+export function useGetUsersAndChatroomInfo(chatroomId: number) {
   return useSuspenseQuery<
     GetUsersOfChatroomQuery,
     GetUsersOfChatroomQueryVariables
-  >(GET_USERS_OF_CHATROOM, {
+  >(GET_USERS_AND_CHATROOM_INFO, {
     variables: { chatroomId },
     skip: !chatroomId, // evita ejecutar si no hay chatroomId
   });
