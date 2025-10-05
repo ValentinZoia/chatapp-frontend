0739be999e34b508ad1efa244b51847de986d99e
ValentinZoia <valentinzoia@gmail.com>
2025-10-05 17:48:41 -0300

    feat(add query getChatroomById)

--- src/graphql/queries/GetUsersOfChatroom.ts
+++ src/graphql/queries/GetUsersOfChatroom.ts
@@ -1,12 +1,22 @@
 import { gql } from "@apollo/client";
 
-export const GET_USERS_OF_CHATROOM = gql`
+export const GET_USERS_AND_CHATROOM_INFO = gql`
   query GetUsersOfChatroom($chatroomId: Float!) {
     getUsersOfChatroom(chatroomId: $chatroomId) {
       id
       fullname
       email
       avatarUrl
+    }
+    getChatroomById(chatroomId: $chatroomId) {
+      id
+      name
+      adminId
+      users {
+        id
+        fullname
+        avatarUrl
+      }
     }
   }
 `;
