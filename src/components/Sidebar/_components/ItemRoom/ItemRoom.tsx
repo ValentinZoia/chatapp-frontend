fc9a5da6f763212456aeecfae9b04e1bce750641
ValentinZoia <valentinzoia@gmail.com>
2025-10-06 19:26:40 -0300

    refactor(import the GetChatroomsForUserSideBarQuery type )

--- src/components/Sidebar/_components/ItemRoom/ItemRoom.tsx
+++ src/components/Sidebar/_components/ItemRoom/ItemRoom.tsx
@@ -1,13 +1,14 @@
 import { SidebarMenuButton, SidebarMenuItem } from "@/components/ui/sidebar";
-import type { GetChatroomsForUserQuery } from "@/gql/graphql";
+import type { GetChatroomsForUserSideBarQuery } from "@/gql/graphql";
 import { Link, useLocation } from "react-router-dom";
 
 function ItemRoom({
   room,
 }: {
-  room: GetChatroomsForUserQuery["getChatroomsForUser"][number];
+  room: GetChatroomsForUserSideBarQuery["getChatroomsForUser"][number];
 }) {
   const location = useLocation();
+
   return (
     <SidebarMenuItem>
       <SidebarMenuButton
@@ -15,7 +16,10 @@ function ItemRoom({
         isActive={location.pathname === `/room/${room.id}`}
       >
         <Link to={`/room/${room.id}`}>
-          <div className={`h-2 w-2 rounded-full`} />
+          <div
+            className="h-2 w-2 rounded-full"
+            style={{ backgroundColor: room.colorHex as string }}
+          />
           <span>{room.name}</span>
         </Link>
       </SidebarMenuButton>
