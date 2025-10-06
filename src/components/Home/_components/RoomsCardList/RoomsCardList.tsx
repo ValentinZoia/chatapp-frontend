b183a0257f2f2275b0cdc8af995595227f7029d0
ValentinZoia <valentinzoia@gmail.com>
2025-10-06 19:30:23 -0300

    feat(add props. And use Filed and RoomCardContent components)

--- src/components/Home/_components/RoomsCardList/RoomsCardList.tsx
+++ src/components/Home/_components/RoomsCardList/RoomsCardList.tsx
@@ -1,14 +1,36 @@
-import { FEATURED_ROOMS } from "@/data/featured-rooms";
 import { RoomCard } from "../RoomCard";
-import { RoomCardContent, RoomCardHeader } from "../RoomCard/_components";
+import {
+  Field,
+  RoomCardContent,
+  RoomCardHeader,
+} from "../RoomCard/_components";
+import type { GetChatroomsForUserQuery } from "@/gql/graphql";
 
-function RoomsCardList() {
+interface RoomsCardListProps {
+  rooms: GetChatroomsForUserQuery["getChatroomsForUser"];
+}
+
+function RoomsCardList({ rooms }: RoomsCardListProps) {
   return (
     <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
-      {FEATURED_ROOMS.map((room) => (
-        <RoomCard key={room.id} roomId={room.id}>
-          <RoomCardHeader room={room} />
-          <RoomCardContent room={room} />
+      {rooms.map((room) => (
+        <RoomCard key={room.id} roomId={room.id as number}>
+          <RoomCardHeader
+            roomName={room.name}
+            roomDescription={room.description}
+          >
+            <Field
+              room={{
+                name: room.name,
+                colorHex: room.colorHex,
+                image: room.image,
+              }}
+            />
+          </RoomCardHeader>
+          <RoomCardContent
+            totalMembers={room.users?.length as number}
+            chatroomId={room.id as number}
+          />
         </RoomCard>
       ))}
     </div>
