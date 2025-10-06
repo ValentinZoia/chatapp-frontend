efee0ea3f9ec03fd2547c0417c80543f64254193
ValentinZoia <valentinzoia@gmail.com>
2025-10-06 19:30:34 -0300

    feat(add props)

--- src/components/Home/_components/RoomsBox/RoomsBox.tsx
+++ src/components/Home/_components/RoomsBox/RoomsBox.tsx
@@ -1,14 +1,18 @@
+import type { GetChatroomsForUserQuery } from "@/gql/graphql";
 import { RoomsCardList } from "../RoomsCardList";
 
-function RoomsBox() {
+interface Props {
+  title: string;
+  rooms: GetChatroomsForUserQuery["getChatroomsForUser"];
+}
+
+function RoomsBox({ title, rooms }: Props) {
   return (
     <div className="space-y-4">
       <div className="flex items-center justify-between">
-        <h2 className="text-xl font-semibold text-foreground">
-          Salas Disponibles
-        </h2>
+        <h2 className="text-xl font-semibold text-foreground">{title}</h2>
       </div>
-      <RoomsCardList />
+      <RoomsCardList rooms={rooms} />
     </div>
   );
 }
