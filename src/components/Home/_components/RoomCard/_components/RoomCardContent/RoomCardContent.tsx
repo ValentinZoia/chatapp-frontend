0ab7c35c362b7434eabb5691bf7800d7f177de02
ValentinZoia <valentinzoia@gmail.com>
2025-10-06 19:32:02 -0300

    feat(add new specific props instead a completed room entity )

--- src/components/Home/_components/RoomCard/_components/RoomCardContent/RoomCardContent.tsx
+++ src/components/Home/_components/RoomCard/_components/RoomCardContent/RoomCardContent.tsx
@@ -1,18 +1,22 @@
 import { BadgeUsersActives } from "@/components/BadgeUsersActives";
-import type { IRoom } from "@/components/Sidebar/_components/ItemRoomList/ItemRoomList";
 import { CardContent } from "@/components/ui/card";
 import { Users } from "lucide-react";
 
-function RoomCardContent({ room }: { room: IRoom }) {
+interface Props {
+  totalMembers: number;
+  chatroomId: number;
+}
+
+function RoomCardContent({ totalMembers, chatroomId }: Props) {
   return (
     <CardContent>
       <div className="flex items-center justify-between">
         <div className="flex items-center gap-4 text-sm text-muted-foreground">
           <div className="flex items-center gap-1">
             <Users className="h-4 w-4" />
-            <span>{room.members}</span>
+            <span>{totalMembers}</span>
           </div>
-          <BadgeUsersActives usersAvtives={room.active} />
+          <BadgeUsersActives chatroomId={chatroomId} />
         </div>
       </div>
     </CardContent>
