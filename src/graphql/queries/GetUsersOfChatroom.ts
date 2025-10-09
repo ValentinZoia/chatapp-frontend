8915f26e56474a95360548c2bac3142dd00c07c4
ValentinZoia <valentinzoia@gmail.com>
2025-10-09 15:58:29 -0300

    refactor(remove unused query)

--- src/graphql/queries/GetUsersOfChatroom.ts
+++ src/graphql/queries/GetUsersOfChatroom.ts
@@ -2,12 +2,12 @@ import { gql } from "@apollo/client";
 
 export const GET_USERS_AND_CHATROOM_INFO = gql`
   query GetUsersOfChatroom($chatroomId: Float!) {
-    getUsersOfChatroom(chatroomId: $chatroomId) {
-      id
-      fullname
-      email
-      avatarUrl
-    }
+    # getUsersOfChatroom(chatroomId: $chatroomId) {
+    #   id
+    #   fullname
+    #   email
+    #   avatarUrl
+    # }
     getChatroomById(chatroomId: $chatroomId) {
       id
       name
