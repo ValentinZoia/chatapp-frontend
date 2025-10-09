83bfc913ab89c26a5d0ceaf807a51a0c044b8c4e
ValentinZoia <valentinzoia@gmail.com>
2025-10-08 23:42:42 -0300

    fix(change the response properties)

--- src/graphql/mutations/AddUsersToChatroom.ts
+++ src/graphql/mutations/AddUsersToChatroom.ts
@@ -2,9 +2,6 @@ import { gql } from "@apollo/client";
 
 export const ADD_USERS_TO_CHATROOM = gql`
   mutation AddUsersToChatroom($chatroomId: Float!, $userIds: [Float!]!) {
-    addUsersToChatroom(chatroomId: $chatroomId, userIds: $userIds) {
-      name
-      id
-    }
+    addUsersToChatroom(chatroomId: $chatroomId, userIds: $userIds)
   }
 `;
