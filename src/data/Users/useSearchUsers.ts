fccbc6aba4fe9a9906d9b97bd12ea0aded1b56df
ValentinZoia <valentinzoia@gmail.com>
2025-10-08 23:43:19 -0300

    refactor(change from useSuspenseQuery to useQuery)

--- src/data/Users/useSearchUsers.ts
+++ src/data/Users/useSearchUsers.ts
@@ -1,16 +1,14 @@
-import { useSuspenseQuery } from "@apollo/client/react";
+// import { useSuspenseQuery } from "@apollo/client/react";
 import { SEARCH_USERS } from "@/graphql/queries";
 import type {
   SearchUsersQuery,
   SearchUsersQueryVariables,
 } from "@/gql/graphql";
+import { useQuery } from "@apollo/client/react";
 
 export function useSearchUsers(fullname: string) {
-  return useSuspenseQuery<SearchUsersQuery, SearchUsersQueryVariables>(
-    SEARCH_USERS,
-    {
-      variables: { fullname },
-      skip: !fullname, // evita ejecutar si no hay userId
-    }
-  );
+  return useQuery<SearchUsersQuery, SearchUsersQueryVariables>(SEARCH_USERS, {
+    variables: { fullname },
+    // skip: !fullname, // evita ejecutar si no hay userId
+  });
 }
