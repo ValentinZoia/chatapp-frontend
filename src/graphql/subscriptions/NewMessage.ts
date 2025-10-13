e8b72a6b13a0a70ea4eee04b2af4e954e2eab021
ValentinZoia <valentinzoia@gmail.com>
2025-10-13 17:06:34 -0300

    feat(add nre properties to newMessage subscripcion response)

--- src/graphql/subscriptions/NewMessage.ts
+++ src/graphql/subscriptions/NewMessage.ts
@@ -3,14 +3,18 @@ import { gql } from "@apollo/client";
 export const NEW_MESSAGE_SUBSCRIPTION = gql`
   subscription NewMessage($chatroomId: Int!) {
     newMessage(chatroomId: $chatroomId) {
-      id
-      content
-      imageUrl
-      user {
+      node {
         id
-        fullname
-        avatarUrl
+        content
+        imageUrl
+        # createdAt
+        user {
+          id
+          fullname
+          avatarUrl
+        }
       }
+      cursor
     }
   }
 `;
