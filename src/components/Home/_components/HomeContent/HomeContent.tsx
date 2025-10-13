bc3dd8abeaa4adca6417f73166393d6e94ed67e4
ValentinZoia <valentinzoia@gmail.com>
2025-10-13 17:08:56 -0300

    refactor(remove Stats cards)

--- src/components/Home/_components/HomeContent/HomeContent.tsx
+++ src/components/Home/_components/HomeContent/HomeContent.tsx
@@ -1,4 +1,3 @@
-import { GenericStatsCard } from "@/components/GenericStatsCard";
 import { FEATURED_ROOMS } from "@/data/featured-rooms";
 import { RoomsBox } from "../RoomsBox";
 import { Suspense } from "react";
@@ -7,29 +6,7 @@ import { UserRoomBox } from "../UserRoomsBox";
 function HomeContent() {
   return (
     <div className="flex-1 overflow-y-auto">
-      <div className="p-8">
-        {/* Stats */}
-        <div className="mb-8 grid gap-4 md:grid-cols-3">
-          <GenericStatsCard
-            title="Total Salas"
-            p="Salas principales activas"
-            numericData={FEATURED_ROOMS.length}
-          />
-          <GenericStatsCard
-            title="Usuarios Activos"
-            p="Conectados ahora"
-            numericData={FEATURED_ROOMS.reduce(
-              (acc, room) => acc + room.active,
-              0
-            )}
-          />
-          <GenericStatsCard
-            title="Más Popular"
-            p={`${FEATURED_ROOMS[0].active} usuarios activos`}
-            numericData="General"
-          />
-        </div>
-
+      <div className="p-8 flex flex-col gap-8">
         {/* Public Rooms Grid */}
         <RoomsBox rooms={FEATURED_ROOMS} title="Salas Principales" />
 
