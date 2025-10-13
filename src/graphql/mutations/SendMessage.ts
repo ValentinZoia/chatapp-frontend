9696a6515c90344a8e287a024f7329a519b8026b
ValentinZoia <valentinzoia@gmail.com>
2025-10-13 17:07:30 -0300

    feat(add new properties to sendMesage mutation response)

--- src/graphql/mutations/SendMessage.ts
+++ src/graphql/mutations/SendMessage.ts
@@ -3,14 +3,18 @@ import { gql } from "@apollo/client";
 export const SEND_MESSAGE = gql`
   mutation SendMessage($chatroomId: Float!, $content: String!, $image: Upload) {
     sendMessage(chatroomId: $chatroomId, content: $content, image: $image) {
-      id
-      content
-      imageUrl
-      user {
+      node {
         id
-        fullname
-        email
+        content
+        imageUrl
+        createdAt
+        user {
+          id
+          fullname
+          avatarUrl
+        }
       }
+      cursor
     }
   }
 `;
