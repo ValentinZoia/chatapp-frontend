66eb46b416084a7bbe878d32d3f1892ab962fafb
ValentinZoia <valentinzoia@gmail.com>
2025-10-24 23:14:51 +0000

    refactor(MainLayout): remove Navbar and MenuBar, and move them to Home Page.

--- src/layouts/MainLayout/MainLayout.tsx
+++ src/layouts/MainLayout/MainLayout.tsx
@@ -1,17 +1,16 @@
 import { Outlet } from "react-router-dom";
 import { SidebarProvider, SidebarInset } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/Sidebar";
-import { NavBar } from "@/components/NavBar";
-import { MenuBar } from "@/components/NavBar/_components";
+
 
 const MainLayout = () => {
   return (
     <SidebarProvider defaultOpen={true}>
       <AppSidebar />
       <SidebarInset>
-        <NavBar />
+
         <Outlet />
-        <MenuBar className="sticky bottom-0 z-40 flex w-full justify-center items-center gap-12 border-t border-muted bg-background text-blue-500  py-6 md:hidden" />
+
       </SidebarInset>
     </SidebarProvider>
   );
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,8 @@
+import { Footer } from "@/components/Footer";
 import { HomeContent } from "@/components/Home/_components";
 import { Image } from "@/components/Image";
+import { NavBar } from "@/components/NavBar";
+import { MenuBar } from "@/components/NavBar/_components/MenuBar";
 
 const banner = {
   imageUrl: "/stadium-banner.png",
@@ -8,46 +11,54 @@ const banner = {
 };
 
 function Home() {
-return (
-    <div className="flex h-full flex-col overflow-hidden bg-muted">
-      <div className="relative w-full overflow-hidden">
-        <Image
-          src={banner.imageUrl || "/placeholder.svg"}
-          alt={banner.title || "Banner"}
-          className="object-cover"
-          aspectRatio={16 / 5}
-          lazy={true}
-          placeholderSrc="https://placehold.co/1000x500"
-          errorSrc="https://placehold.co/1000x500"
-        />
-        {banner.title && (
-          <div className="absolute inset-0 bg-gradient-to-r from-black/50 to-transparent flex flex-col justify-center p-10 lg:p-14">
-            <h2 className="text-3xl md:text-5xl font-bold text-white mb-2">
-              {banner.title}
-            </h2>
-            <p className="text-white/90 text-lg md:text-xl mb-6 max-w-md">
-              {banner.description}
-            </p>
-          </div>
-        )}
-      </div>
 
-      {/* <SidebarTrigger /> */}
-      {/* Header */}
-      {/* <div className="border-b border-border bg-card px-8 py-6">
-        <div className="space-y-2">
-          <h1 className="text-3xl font-bold tracking-tight text-foreground">
-            Bienvenido al Chat de Fútbol Argentino
-          </h1>
-          <p className="text-muted-foreground">
-            Elegí una sala y empezá a charlar con otros hinchas
-          </p>
+  return (
+    <>
+
+      <NavBar />
+
+      <div className="flex h-full flex-col overflow-hidden bg-muted mb-2">
+        <div className="relative w-full overflow-hidden">
+          <Image
+            src={banner.imageUrl || "/placeholder.svg"}
+            alt={banner.title || "Banner"}
+            className="object-cover"
+            aspectRatio={16 / 5}
+            lazy={true}
+            placeholderSrc="https://placehold.co/1000x500"
+            errorSrc="https://placehold.co/1000x500"
+          />
+          {banner.title && (
+            <div className="absolute inset-0 bg-gradient-to-r from-black/50 to-transparent flex flex-col justify-center p-10 lg:p-14">
+              <h2 className="text-3xl md:text-5xl font-bold text-white mb-2">
+                {banner.title}
+              </h2>
+              <p className="text-white/90 text-lg md:text-xl mb-6 max-w-md">
+                {banner.description}
+              </p>
+            </div>
+          )}
         </div>
-      </div> */}
 
-      {/* Content */}
-      <HomeContent />
-    </div>
+        {/* <SidebarTrigger /> */}
+        {/* Header */}
+        {/* <div className="border-b border-border bg-card px-8 py-6">
+        <div className="space-y-2">
+        <h1 className="text-3xl font-bold tracking-tight text-foreground">
+        Bienvenido al Chat de Fútbol Argentino
+        </h1>
+        <p className="text-muted-foreground">
+        Elegí una sala y empezá a charlar con otros hinchas
+        </p>
+        </div>
+        </div> */}
+
+        {/* Content */}
+        <HomeContent />
+        <Footer />
+      </div>
+      <MenuBar className="sticky bottom-0 z-40 flex w-full justify-center items-center gap-12 border-t border-muted bg-background text-blue-500  py-6 md:hidden" />
+    </>
   );
 }
 export default Home;
