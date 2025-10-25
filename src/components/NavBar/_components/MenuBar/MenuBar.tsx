915dd4d3eef64fc0c48683773c6de1cd032aed8f
ValentinZoia <valentinzoia@gmail.com>
2025-10-25 01:54:16 +0000

    feat(MenuBar) Add new icons

--- src/components/NavBar/_components/MenuBar/MenuBar.tsx
+++ src/components/NavBar/_components/MenuBar/MenuBar.tsx
@@ -1,4 +1,4 @@
-import { Home, User2 } from "lucide-react";
+import { Home, CirclePlus, MessagesSquare, User2 } from "lucide-react";
 import { Link } from "react-router-dom";
 
 interface MenuBarProps {
@@ -9,20 +9,32 @@ export default function MenuBar({ className }: MenuBarProps) {
   return (
     <>
       <div className={className}>
-        <Link to="/" aria-label="Ir al inicio">
-          <Home
-            className="h-6 w-6"
-            aria-hidden="true" // Atributo ARIA: mejora la accesibilidad
-          />
+
+        {/* Inicio */}
+        <Link to="/" className="flex flex-col items-center  hover:text-foreground transition-colors">
+          <Home className="h-6 w-6" aria-hidden="true" />
+          <span className="text-xs mt-1">Inicio</span>
         </Link>
 
-        <Link to="/" aria-label="Ir al historial de partidos">
-          <User2
-            className="h-6 w-6"
-            aria-hidden="true" // Atributo ARIA: mejora la accesibilidad
-          />
+        {/* Crear sala */}
+        <Link to="/crear" className="flex flex-col items-center hover:text-foreground transition-colors">
+          <CirclePlus className="h-6 w-6" aria-hidden="true" />
+          <span className="text-xs mt-1">Nueva Sala</span>
+        </Link>
+
+        {/* Mis salas */}
+        <Link to="/mis-salas" className="flex flex-col items-center  hover:text-foreground transition-colors">
+          <MessagesSquare className="h-6 w-6" aria-hidden="true" />
+          <span className="text-xs mt-1">Mis Salas</span>
+        </Link>
+
+        {/* Perfil de usuario */}
+        <Link to="/" className="flex flex-col items-center  hover:text-foreground transition-colors" aria-label="Perfil de usuario">
+          <User2 className="h-6 w-6" aria-hidden="true" />
+          <span className="text-xs mt-1">Mi Perfil</span>
         </Link>
       </div>
+
     </>
   );
 }
