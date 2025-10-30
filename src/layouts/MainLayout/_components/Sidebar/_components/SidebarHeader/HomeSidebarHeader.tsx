6f66a4f5be687ffb72ea7d3076fb2d7218acfaaa
ValentinZoia <valentinzoia@gmail.com>
2025-10-30 01:58:53 +0000

    feat(HomeSidebarHeader): Add Futbol Chat icon to the sidebar header

--- src/layouts/MainLayout/_components/Sidebar/_components/SidebarHeader/HomeSidebarHeader.tsx
+++ src/layouts/MainLayout/_components/Sidebar/_components/SidebarHeader/HomeSidebarHeader.tsx
@@ -1,17 +1,21 @@
 import { SidebarHeader } from "@/components/ui/sidebar";
-import { MessageSquare } from "lucide-react";
+
 
 function HomeSidebarHeader() {
   return (
     <SidebarHeader className="border-b border-sidebar-border">
       <div className="w-full flex items-center justify-between gap-3 px-2 py-2">
-        <div className="flex gap-2">
-          <div className="flex h-10 w-10 items-center justify-center rounded-lg bg-primary">
-            <MessageSquare className="h-5 w-5 text-primary-foreground" />
+        <div className="flex items-center gap-2">
+          <div className="flex h-10 w-10 items-center justify-center rounded-lg">
+            <img
+              src="/icon0.svg"
+              alt="logo"
+              className="aspect-auto object-cover"
+            />
           </div>
           <div className="flex flex-col">
             <h1 className="text-lg font-bold leading-none text-sidebar-foreground">
-              Fútbol Chat
+              Futbol Chat
             </h1>
             <p className="text-xs text-muted-foreground">Argentina</p>
           </div>
