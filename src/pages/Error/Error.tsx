d56e1336ed493fab97aef2080cce8ee8e33f4b7d
ValentinZoia <valentinzoia@gmail.com>
2025-11-15 02:10:58 +0000

    refactor(ErrorPage): redesign the page with logo and colors

--- src/pages/Error/Error.tsx
+++ src/pages/Error/Error.tsx
@@ -1,15 +1,26 @@
+import { Button } from "@/components/ui/button";
 import { Link } from "react-router-dom";
 
 const Error = () => {
   return (
     <>
-      <div className="w-full min-h-screen flex flex-col">
-        <div className="w-full flex-1 flex flex-col gap-4 justify-center items-center">
-          <h2 className="text-2xl font-bold">
-            Parece que esta página no existe
+      <div className="flex flex-col gap-4 h-screen w-full items-center justify-center">
+        <div className="flex h-10 w-10 items-center justify-center rounded-lg">
+          <img
+            src="/icon0.svg"
+            alt="logo"
+            className="aspect-auto object-cover"
+          />
+        </div>
+        <div className="text-center">
+          <h2 className="text-2xl font-bold text-foreground">
+            Ops! Ocurrio un error
           </h2>
-          <Link to="/" className="text-muted-foreground ml-2">
-            Volver al inicio
+          <p className="mt-2 text-muted-foreground">
+            La sala que buscás no existe o no estas autorizado para verla.
+          </p>
+          <Link to="/">
+            <Button className="cursor-pointer mt-4 bg-blue-400 hover:bg-blue-400/80">Volver al inicio</Button>
           </Link>
         </div>
       </div>
